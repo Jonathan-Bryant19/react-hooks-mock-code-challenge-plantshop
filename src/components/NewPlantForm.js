a52364c7d79c7d38a7e8f71eb82a311355416b7a
Jonathan Bryant <spectrum19tech@gmail.com>
2022-02-08 07:26:27 -0800

    Second commit

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm( {handleOnSubmit}) {
+  const [formName, setFormName] = useState("")
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      <form onSubmit={handleOnSubmit}>
         <input type="text" name="name" placeholder="Plant name" />
         <input type="text" name="image" placeholder="Image URL" />
         <input type="number" name="price" step="0.01" placeholder="Price" />
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -12,13 +12,22 @@ function PlantPage() {
       .then(setData)
   }, [])
 
-  
+  function handleOnSubmit(e) {
+    e.preventDefault()
+    const formData = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      price: e.target.price.value
+    }
+    setData([...data, formData])
+    console.log(data)
+  }
 
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm handleOnSubmit={handleOnSubmit}/>
       <Search />
-      <PlantList data={data}/>
+      <PlantList data={data} setData={setData}/>
     </main>
   );
 }
